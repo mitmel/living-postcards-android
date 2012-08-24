e781891fcf3f4bafea7141a405aedb6ea2a43f41
Steve Pomeroy <steve@staticfree.info>
2012-08-24 18:03:33 -0400

    Fixes AspectRatioImageView so it respects maxWidth, maxHeight

--- src/com/stackoverflow/android/widget/AspectRatioImageView.java
+++ src/com/stackoverflow/android/widget/AspectRatioImageView.java
@@ -1,5 +1,7 @@
 package com.stackoverflow.android.widget;
 
+import java.lang.reflect.Field;
+
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -10,25 +12,53 @@ import android.widget.ImageView;
  *
  * To use, set the width/height to 0dip of the side that you wish to resize. It will then be
  * adjusted based on the aspect ratio of the image.
- * 
+ *
  * {@link http
  * ://stackoverflow.com/questions/2991110/android-how-to-stretch-an-image-to-the-screen-width
  * -while-maintaining-aspect-ra/2999707}
- * 
- * 
+ *
+ *
  */
 public class AspectRatioImageView extends ImageView {
 
+	int mMaxWidth = Integer.MAX_VALUE;
+	int mMaxHeight = Integer.MAX_VALUE;
+
 	public AspectRatioImageView(Context context) {
 		super(context);
+		init(context, null);
 	}
 
 	public AspectRatioImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init(context, attrs);
 	}
 
 	public AspectRatioImageView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+		init(context, attrs);
+	}
+
+	private void init(Context context, AttributeSet attrs) {
+		// http://stackoverflow.com/questions/8311081/how-to-get-maxwidth-and-maxheight-parameters-of-imageview
+		// haaaaack
+		try {
+			final Field maxWidthField = ImageView.class.getDeclaredField("mMaxWidth");
+			final Field maxHeightField = ImageView.class.getDeclaredField("mMaxHeight");
+			maxWidthField.setAccessible(true);
+			maxHeightField.setAccessible(true);
+
+			mMaxWidth = (Integer) maxWidthField.get(this);
+			mMaxHeight = (Integer) maxHeightField.get(this);
+		} catch (final SecurityException e) {
+			// we don't care if we can't get it. We weren't really supposed to anyhow.
+		} catch (final NoSuchFieldException e) {
+			// we don't care if we can't get it. We weren't really supposed to anyhow.
+		} catch (final IllegalArgumentException e) {
+			// we don't care if we can't get it. We weren't really supposed to anyhow.
+		} catch (final IllegalAccessException e) {
+			// we don't care if we can't get it. We weren't really supposed to anyhow.
+		}
 	}
 
 	@Override
@@ -45,7 +75,7 @@ public class AspectRatioImageView extends ImageView {
 
 				final int diw = drawable.getIntrinsicWidth();
 				if (diw > 0) {
-					height = width * drawable.getIntrinsicHeight() / diw;
+					height = Math.min(width * drawable.getIntrinsicHeight() / diw, mMaxHeight);
 					setMeasuredDimension(width, height);
 					setMeasuredDimension = true;
 				}
@@ -54,7 +84,7 @@ public class AspectRatioImageView extends ImageView {
 
 				final int dih = drawable.getIntrinsicHeight();
 				if (dih > 0) {
-					width = height * drawable.getIntrinsicWidth() / dih;
+					width = Math.min(height * drawable.getIntrinsicWidth() / dih, mMaxWidth);
 					setMeasuredDimension(width, height);
 					setMeasuredDimension = true;
 				}
