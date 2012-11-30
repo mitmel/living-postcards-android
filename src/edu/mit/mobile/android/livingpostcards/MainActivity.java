d8f5ffec5c7ff486ddfd9fe34fb46288498bd3ee
Steve Pomeroy <spomeroy@mit.edu>
2012-11-30 16:26:14 -0500

    Bumps SDK number; other minor items

--- src/edu/mit/mobile/android/livingpostcards/MainActivity.java
+++ src/edu/mit/mobile/android/livingpostcards/MainActivity.java
@@ -190,15 +190,18 @@ public class MainActivity extends SherlockFragmentActivity implements OnCreateOp
 
         final String tag = (String) tab.getTag();
 
+        // first remove anything that's not what we want
         if (f != null && !tag.equals(f.getTag())) {
             ft.detach(f);
             f = null;
         }
 
+        // search to see if there's one stashed away
         if (f == null) {
             f = fm.findFragmentByTag(tag);
         }
 
+        // and reattach / create anything
         if (f != null) {
             ft.attach(f);
         } else {
